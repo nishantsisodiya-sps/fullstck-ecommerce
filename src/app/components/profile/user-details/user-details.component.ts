85c2671904c7c29f733032e12d6e6643c036b692
nishantsisodiya-sps <nishantsisodiya.softprodigy@gamil.com>
2023-05-24 19:17:48 +0530

    profile created

--- src/app/components/profile/user-details/user-details.component.ts
+++ src/app/components/profile/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthTokenService } from 'src/app/service/auth-token.service';
+import { UserAuthApiService } from 'src/app/service/user-auth-api.service';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  profile : any = []
+  isEditMode: boolean = false;
+  constructor(private api : UserAuthApiService , private auth : AuthTokenService ) { }
 
   ngOnInit(): void {
+    this.getProfile()
+  }
+
+
+  getProfile(){
+
+    let user = this.auth.getSellerId()
+    let id = user.id
+    let role = user.role
+
+    if(role === 'seller'){
+      this.api.getSellerProfile(id).subscribe(response=>{
+        this.profile.push(response)
+    })
+    }
+    else if(role === 'user'){
+      this.api.getUserProfile(id).subscribe(res=>{
+        this.profile.push(res)
+        console.log(this.profile);
+        
+      })
+    }
+
+  }
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
   }
 
 }
